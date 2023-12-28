4bee48db43a0e4e574443d2e78db078216de919a
kanesya12 <154325690+kanesya12@users.noreply.github.com>
2023-12-28 10:09:05 +0700

    Create barang.js

---
+++ barang.js
@@ -0,0 +1,16 @@
+const barang = ["kode barang" = 0o1, "nama barang" = A, "harga" = 10.000,
+                "kode barang" = 0o2, "nama barang" = B, "harga" = 20.000,
+                "kode barang" = 0o3, "nama barang" = C, "harga" = 30.000 ]
+
+if (for (let index = 0; index < array.length; index++) {
+    const name = array[index];
+    condition
+}) {
+    
+backgroundcolor red 
+alert localStorage : "localhostssay"
+var TextTrackList :{
+    const barang = new type(arguments);
+} 
+
+
